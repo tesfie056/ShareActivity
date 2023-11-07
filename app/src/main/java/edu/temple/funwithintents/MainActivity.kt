e9499e2447971cc30f2e2383c27909d953e75d82
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2023-11-07 14:50:57 -0500

    created onclick listner to be able to share when it is clicked

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -16,6 +17,51 @@ class MainActivity : AppCompatActivity() {
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
 
+            val intent = Intent().apply {
+                intent.action = Intent.ACTION_SEND
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_TEXT, editText.text)
+
+            }
+startActivity(Intent.createChooser(intent, null))
+
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//val share_text_1_btn = findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.share_text_1_btn)
+//share_text_1_btn.setOnClickListener {
+//    val intent= Intent()
+//    intent.action=Intent.ACTION_SEND
+//    intent.putExtra(Intent.EXTRA_TEXT,"Hey Check out this Great app:")
+//override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//share_text_1_btn.setOnClickListener {
+//            val t1 = share_text_1_t.text.toString()
+//            val shareIntent = Intent()
+//            shareIntent.action = Intent.ACTION_SEND
+//            shareIntent.type="text/plain"
+//            shareIntent.putExtra(Intent.EXTRA_TEXT, t1)
+//            startActivity(Intent.createChooser(shareIntent,"Share via"))
+//        }
+//}
